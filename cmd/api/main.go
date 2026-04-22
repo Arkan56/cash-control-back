b8766e9a95064f9c7264ab6306e13d743090ed97
Arkan56 <seb4s.s44@gmail.com>
2026-04-22 06:59:49 -0500

    refactor: reorganize project structure

---
+++ cmd/api/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"cash-control/internal/config"
+	"cash-control/internal/database"
+	"fmt"
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func main() {
+	var cfg *config.Config
+	var err error
+	cfg, err = config.Load()
+
+	if err != nil {
+		log.Fatal("Failed to load configuration:", err)
+	}
+
+	var pool *pgxpool.Pool
+	pool, err = database.Connect(cfg.DatabaseURL)
+
+	if err != nil {
+		log.Fatal("Failde to connect to database:", err)
+	}
+
+	defer pool.Close()
+
+	fmt.Println("API Server")
+	var r *gin.Engine = gin.Default()
+
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message":  "it's ok",
+			"databese": "connected",
+		})
+	})
+
+	r.Run(":" + cfg.Port)
+
+}
